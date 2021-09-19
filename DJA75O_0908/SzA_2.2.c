9f30727029bdf84c842f893c72fb42605aa0282f
Sz-Attila03 <71133968+Sz-Attila03@users.noreply.github.com>
2021-09-19 17:19:13 +0200

    Create SzA_2.2.c

---
+++ DJA75O_0908/SzA_2.2.c
@@ -0,0 +1,43 @@
+// /mnt/d/github/DJA75O_DB1Gyak/DJA75O_0908
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fp;
+    fp = fopen("munka.txt", "w");
+
+    /////////////////////////////////////////////////////
+
+    char be;
+    char seged;
+    char szoveg[100];
+
+    /////////////////////////////////////////////////////
+
+    printf("2. feldat:\n");
+    printf("Adja meg a szoveget:\n");
+
+    while( (be = getchar()) != '#' )
+    {
+        putc(be, fp);
+    }
+
+    fclose(fp);
+
+    /////////////////////////////////////////////////////
+
+    fp = fopen("munka.txt", "r");
+
+    while( (be = getc(fp)) != EOF )
+    {
+        printf("%c", toupper(be) );
+    }
+
+    fclose(fp);
+
+    return 0;
+}

d4eba2eae96138571539ac8a781ab574e86ae156
Sz-Attila03 <71133968+Sz-Attila03@users.noreply.github.com>
2021-09-30 12:34:23 +0200

    Create DJA75O_3_3.java

---
+++ DJA75O_0929/DJA75O_3.3/src/feladat_3/DJA75O_3_3.java
@@ -0,0 +1,56 @@
+package feladat_3;
+
+import java.io.*;
+
+public class DJA75O_3_3 {
+
+	public static void main(String[] args) {
+		String sor;
+		String[] szavak;
+		int id = 0;
+		
+		try
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = null;
+			
+			while (id >= 0) 
+			{
+				sor = br.readLine();
+				
+				if(id == 0)
+				{
+					bw = new BufferedWriter(new FileWriter(sor)); 
+				}
+				else
+				{
+					bw.write(sor);
+					bw.newLine();
+				}
+				
+				id += 1;
+				szavak = sor.split(" ");
+				
+				for(String sz : szavak)
+				{
+					System.out.println(sz + ":");
+					
+					if(sz.compareTo("end") == 0)
+					{
+						br.close();
+						id = -1;
+					}
+				}
+			}
+			
+			bw.close();
+			
+			System.out.println("Kész");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}

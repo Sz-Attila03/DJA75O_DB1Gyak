65452afddb6af8e1e7d67d851f53c92c5bcd61b6
Sz-Attila03 <71133968+Sz-Attila03@users.noreply.github.com>
2021-11-22 15:43:39 +0100

    Update 9.GyakDJA75O_seged.sql

--- DJA75O_1117/9.GyakDJA75O_seged.sql
+++ DJA75O_1117/9.GyakDJA75O_seged.sql
@@ -17,3 +17,42 @@ WHERE termek.gyarto IS NULL;
 //1.3
 INSERT INTO alkatresz VALUES (25864, "Intel Core I5", 80000);
 INSERT INTO komponens VALUES (4521, 25864);
+
+//2.1
+INSERT INTO tanfolyam VALUES (002, 110000, "informatika", "a");
+INSERT INTO tanfolyam VALUES (003, 115000, "gazdaság", "b");
+INSERT INTO tanfolyam VALUES (004, 50000, "földmérő", "c");
+INSERT INTO tanfolyam VALUES (005, 120000, "informatika", "d");
+INSERT INTO tanfolyam VALUES (006, 130000, "informatika", "d");
+INSERT INTO tanfolyam VALUES (007, 70000, "földmérő", "c");
+INSERT INTO tanfolyam VALUES (008, 118000, "gazdaság", "b");
+
+SELECT tanfolyam.tipus, AVG(tanfolyam.ar)
+FROM tanfolyam
+GROUP BY tanfolyam.tipus;
+
+//2.2
+SELECT tanfolyam.tipus, tanfolyam.ar,
+CASE
+	WHEN tanfolyam.ar > 100000 Then "draga"
+    ELSE "olcso"
+END
+FROM tanfolyam;
+
+//2.3
+
+//3.1
+CREATE TABLE gyartotermek_kapcsolo(
+	gyarto_id INT,
+	FOREIGN KEY (gyarto_id) REFERENCES gyarto.adoszam,
+	termek_id INT,
+	FOREIGN KEY (termek_id) REFERENCES termek.tkod
+);
+
+SELECT gyarto.*, termek.*
+FROM gyarto
+JOIN gyartotermek_kapcsolo ON gyarto.adoszam = termek.tkod
+JOIN termek ON gyartotermek_kapcsolo.termek_id = termek.tkod
+
+//4.1
+ALTER TABLE termek ADD (kategoria CHAR(40));
